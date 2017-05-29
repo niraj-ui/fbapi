cf1e0f1bce540316eaf248cca3e9092fee65daa7
Niraj Kumar <nirajbca2011@gmail.com>
2017-05-29 17:09:08 +0530

    Update profile.js

--- profile.js
+++ profile.js
@@ -3,7 +3,7 @@
 $(Document).ready(function(){
    $('.loader').hide();
 	
-var myfbtoken='EAACEdEose0cBAFj1PUofU1KM8k97wTGZAoDc3KxtKe9VILnPZCaZChcmX9LSg1tGEVyZB7ayAVZAPP0dTGALOZAtAfv8S4bZBC1Ewk2C500eygac9v6Yuld3rmIQBZBwlQN4QvANaKcptwit7RCZCWFcyEYsZA3ZCl01jN5ffznZA1hKF4xORZA3O88Y5gWZC7uQiNz1IZD'
+var myfbtoken='EAACEdEose0cBACicJvNlwADLmdp9huLfRA3xFvZAO3g2O9ERAkbS6vqaTAVZAtspv8OPWaQDs0jC8uMjAKTtmY4bK4xWxBDKxfYCDhLZAXIPwgCnJrbMVLpy8hDUm0ZCYuieeroWgrvq7HVzgvuZB2x2i2oAT304Ah7wAd3owW0H1RXOyEBCpFGCaCkAPwsUZD'
     
     function getfbinfo(){
     $.ajax('https://graph.facebook.com/me?access_token=' + myfbtoken, {
@@ -14,7 +14,7 @@ var myfbtoken='EAACEdEose0cBAFj1PUofU1KM8k97wTGZAoDc3KxtKe9VILnPZCaZChcmX9LSg1tG
             $('#profileid').html('<a target="blank" href="https://www.fb.com/' + response.id + ' "> '+response.id +'   </a>');
             $('#fir_name').text(response.first_name);
             $('#las_name').text(response.last_name);
-         //   $('#edu').text(response.education.school.name);
+          
             $('#loct').text(response.location.name);
             $('#birthday').text(response.birthday);
             $('#schname').text(response.email);
@@ -24,8 +24,7 @@ var myfbtoken='EAACEdEose0cBAFj1PUofU1KM8k97wTGZAoDc3KxtKe9VILnPZCaZChcmX9LSg1tG
             $('#languages').text(response.languages.name);
             $('#relation').text(response.relationship_status);
             $('#religion').text(response.religion);
-            
-         //   $('#employer').text(response.work.employer.name);
+   	   $('#employer').text(response.work.employer.name);
            
         
             
